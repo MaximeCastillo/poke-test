774d066f5cd3f4ab71c58b5336c794b37fed63a8
Maxime CASTILLO <maxime.castillo@gmail.com>
2021-09-27 03:07:17 +0200

    root to api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: redirect('/api/v1/pokemons')
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
       get "/pokemons", to: "pokemons#index"
